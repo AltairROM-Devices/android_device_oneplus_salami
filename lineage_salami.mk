f22567f412a4311658ecf9761812d0f038c6cdbc
Martin J. Pollard <martin0566@gmail.com>
2025-10-12 16:31:27 -0400

    salami: Integrate Google Apps core components
    Based on MindTheGapps.

--- lineage_salami.mk
+++ lineage_salami.mk
@@ -13,6 +13,9 @@ $(call inherit-product, device/oneplus/salami/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit Google Apps core.
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
 PRODUCT_NAME := lineage_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
